0bddcd1b7ca5d27805dcf54899f800f75b300e46
Geovana Vitória Silva Diorio <150198349+diorio8@users.noreply.github.com>
2026-01-28 01:59:17 -0300

    adiciona função para adicionar tarefas e teste inicial
    - criada lista de tarefas
    - função adicionar_tarefa com verificação de entrada
    -testadas tarefas de exemplo
    -adicionado print final para mostrar todas as tarefas
    -revisada indentação e docstrings

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 # Programa de gerenciador de tarefas
 
 # Listas de tarefas
+
 tarefas = []
+# Função para adicionar tarefas
+
+def adicionar_tarefa(tarefa):
+  """ 
+  adiciona uma nova tarefa a lista de tarefas.
+  parâmetro:
+  tarefa (str): texto da tarefa a ser adicionada
+  """
+  if tarefa:    # verificar se a tarefa não está vazia
+    tarefas.append(tarefa)
+    print(f"Tarefa '{tarefa}' adicionada com sucesso!")
+  else:
+    print("Não é possivel adicionar uma tarefa.")
+  
+# chamadas da função
+adicionar_tarefa("Estudar Python - funções e listas")
+adicionar_tarefa("Fazer exercícios de git e github")
+adicionar_tarefa("Atualizar perfil LinkedIn")
+adicionar_tarefa("Planejar cronograma semanal de estudos")
+adicionar_tarefa("Revisar código do projeto")
+adicionar_tarefa("Testar nova funcionalidades de algum projeto novo")
+
+# mostrar todas as tarefas
+print(tarefas)
